99ee5afb3389d4167e805fc5af5887be0a42fb0b
EJ Frias <ejsfrias@gmail.com>
2016-05-22 00:24:30 +0800

    Added bootstrap-dametimepicker-angular file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # bootstrap-datetimepicker-angular
 Angular 1.5.x Component for Eonasdan's bootstrap-datetimepicker
+
+
+### Demo
+[https://ejfrias.github.io/bootstrap-datetimepicker-angular](https://ejfrias.github.io/bootstrap-datetimepicker-angular)
+
+
+### Requirements
+[https://jquery.com/](jQuery)
+[https://github.com/twbs/bootstrap](Bootstrap)
+[https://github.com/Eonasdan/bootstrap-datetimepicker](Eonasdan's date/time picker widget)
+
+
+### How to use
+Just add a bootstrap-datetimepicker-angular.js after bootstrap-datetimepicker.js:
+```
+<script src="/path/to/bootstrap-datetimepicker-angular.js"></script>
+```
+
+Then, add the `<date-time-picker><date-time-picker>` component anywhere on your app:
+```
+<script src="/path/to/tooltip.js"></script>
+```
+
+
+### Options
+
+Name | Type | Required | Description
+---- | ---- | ------- | -----------
+ng-model | string | required | Scope property.
+placeholder | string | optional | Placeholder of the input element.
---
+++ bootstrap-datetimepicker-angular.js
@@ -0,0 +1,51 @@
+angular.module('bootstrap-datetimepicker-angular', [])
+
+.component('dateTimePicker', {
+	bindings: {
+		model: '@ngModel',
+        placeholder: '@'
+	},
+	template: [
+		'<div class="input-group date">',
+			'<input type="text" class="form-control" placeholder="{{$ctrl.placeholder}}" />',
+			'<span class="input-group-addon clickable">',
+				'<i class="glyphicon glyphicon-calendar"></i>',
+			'</span>',
+		'</div>'
+	].join(''),
+	controller( $scope, $element, $attrs ){
+		var $this = this;
+		var picker = $('div.date', $element);
+		var input = $('input', $element);
+		
+		picker.datetimepicker({
+			useCurrent: false, //Important! See issue #1075
+			allowInputToggle: true,
+			format: 'YYYY-MM-DD'
+		});
+		
+		if( $attrs.link && $attrs.linktype ){
+			picker.on('dp.change', function(e){
+				var el = $('div.date', '#'+$attrs.link).data("DateTimePicker");
+				
+				if( $attrs.linktype == 'min' ){
+					el.minDate( e.date );
+				}
+				else if( $attrs.linktype == 'max' ){
+					el.maxDate( e.date );
+				}
+			});
+		}
+		
+		input.on('blur', function(e){
+			var parts = $this.model.split('.');
+			
+			if( parts.length > 1 ){
+				$scope.$parent[parts[0]][parts[1]] = input.val();
+			}
+			else {
+				$scope.$parent[parts[0]] = input.val();
+			}
+		});
+	}
+})
